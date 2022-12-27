a845b9fd80e285e9d1ba668f7ec95678392bebdf
Paulo Sergio Campos de Lima <cloverfieldor@gmail.com>
2022-12-27 15:07:51 -0300

    Supporting multiple colleges

--- main.py
+++ main.py
@@ -1,35 +1,31 @@
 import requests
-import pyexcel as pe
 import os
-import json
 from copy import copy
 from string import ascii_uppercase as alc
 from openpyxl import load_workbook, Workbook
-#os.remove('nove_conceito_enade.xlsx')
-#year = input('Me de o ano que você quer checar')
 path_enade = '/home/paulolima/get_best_colleges/conceito_enade.xlsx'
 path_idd = '/home/paulolima/get_best_colleges/conceito_idd.xlsx'
 year = 2021
 url_link = f'https://download.inep.gov.br/educacao_superior/indicadores/resultados/{year}'
 url_link_enade = f'{url_link}/conceito_enade_{year}.xlsx'
 url_link_idd = f'{url_link}/IDD_{year}.xlsx'
-print(url_link_enade)
+courses = ['CIÊNCIA DA COMPUTAÇÃO', 'TECNOLOGIA EM ANÁLISE E DESENVOLVIMENTO DE SISTEMAS', 'SISTEMAS DE INFORMAÇÃO']
 if not os.path.exists(path_enade):
     conceito_enade = requests.get(url_link_enade, verify=False)
     open("conceito_enade.xlsx", "wb").write(conceito_enade.content)
 if not os.path.exists(path_idd):
-    print("hallo")
     conceito_idd = requests.get(url_link_idd, verify=False)
     open("conceito_idd.xlsx", "wb").write(conceito_idd.content)
 
 try:
     os.remove('novo_conceito_enade.xlsx')
 except:
-    print('First time')
+    print('File already on the system')
+
 try:
-    os.remove('novo_coneito_enade.xlsx')
+    os.remove('novo_conceito_idd.xlsx')
 except:
-    print("first time")
+    print("File already on the system")
 enade_wb = load_workbook(filename='conceito_enade.xlsx')
 idd_wb = load_workbook(filename='conceito_idd.xlsx')
 
@@ -50,7 +46,6 @@ def create(type='enade'):
     for i in alc:
         header_array.append(enade_ws[f'{i}1'].value)
         new_enade_ws.column_dimensions[i] = enade_ws.column_dimensions[i]
-        print('test',i)
 
     new_enade_ws.append(header_array)
 
@@ -61,18 +56,15 @@ def create(type='enade'):
         new_enade_ws[f'{i}1'].alignment = copy(enade_ws[f'{i}1'].alignment)
     #print(enade_ws.rows)
     for data in enade_ws['C']:
-        #print(data.value)
-        if data.value != None and data.value.strip() == "CIÊNCIA DA COMPUTAÇÃO":
-            #print(data.row)
+        try:
+            courses.index(data.value.strip())
             enade_array.append(data.row)
-
-
+        except:
+            pass
     for data in enade_array:
         cool_array = []
         for i in alc:
-            #print(enade_ws[f'{i}{data}'].value)
             cool_array.append(enade_ws[f'{i}{data}'].value)
-        #print(cool_array)
         new_enade_ws.append(cool_array)
     new_enade.save(filename=filename)
 

ee9c3456796a85ed40aba23b61d4831fb1cb2678
Paulo Sergio Campos de Lima <cloverfieldor@gmail.com>
2022-12-28 12:38:56 -0300

    Fixed styling error with idd 2018

--- main.py
+++ main.py
@@ -2,20 +2,26 @@ import requests
 import os
 from copy import copy
 from string import ascii_uppercase as alc
-from openpyxl import load_workbook, Workbook
-path_enade = '/home/paulolima/get_best_colleges/conceito_enade.xlsx'
-path_idd = '/home/paulolima/get_best_colleges/conceito_idd.xlsx'
-year = 2021
+from openpyxl import load_workbook, Workbook, styles
+year = input("What year you want to check? \n")
+path_enade = f'/home/paulolima/get_best_colleges/conceito_enade{year}.xlsx'
+path_idd = f'/home/paulolima/get_best_colleges/conceito_idd{year}.xlsx'
 url_link = f'https://download.inep.gov.br/educacao_superior/indicadores/resultados/{year}'
 url_link_enade = f'{url_link}/conceito_enade_{year}.xlsx'
 url_link_idd = f'{url_link}/IDD_{year}.xlsx'
-courses = ['CIÊNCIA DA COMPUTAÇÃO', 'TECNOLOGIA EM ANÁLISE E DESENVOLVIMENTO DE SISTEMAS', 'SISTEMAS DE INFORMAÇÃO']
+if year == '2018':
+    url_link_enade = 'https://download.inep.gov.br/educacao_superior/indicadores/legislacao/2019/resultados_conceito_enade_2018.xlsx'
+    url_link_idd = 'https://download.inep.gov.br/educacao_superior/indicadores/legislacao/2019/resultados_IDD_2018.xlsx'
+elif year == '2019':
+    url_link_enade = f'{url_link}/Conceito_Enade_2019.xlsx'
+    url_link_idd = f'{url_link}/IDD_2019.xlsx'
+courses = ['CIÊNCIA DA COMPUTAÇÃO', 'TECNOLOGIA EM ANÁLISE E DESENVOLVIMENTO DE SISTEMAS', 'SISTEMAS DE INFORMAÇÃO', 'MEDICINA VETERINÁRIA']
 if not os.path.exists(path_enade):
     conceito_enade = requests.get(url_link_enade, verify=False)
-    open("conceito_enade.xlsx", "wb").write(conceito_enade.content)
+    open(f"conceito_enade{year}.xlsx", "wb").write(conceito_enade.content)
 if not os.path.exists(path_idd):
     conceito_idd = requests.get(url_link_idd, verify=False)
-    open("conceito_idd.xlsx", "wb").write(conceito_idd.content)
+    open(f"conceito_idd{year}.xlsx", "wb").write(conceito_idd.content)
 
 try:
     os.remove('novo_conceito_enade.xlsx')
@@ -26,13 +32,16 @@ try:
     os.remove('novo_conceito_idd.xlsx')
 except:
     print("File already on the system")
-enade_wb = load_workbook(filename='conceito_enade.xlsx')
-idd_wb = load_workbook(filename='conceito_idd.xlsx')
-
+enade_wb = load_workbook(filename=f'conceito_enade{year}.xlsx')
+enade_ws2 = enade_wb.active
+dead_style = copy(enade_ws2['A1'].style)
+idd_wb = load_workbook(filename=f'conceito_idd{year}.xlsx')
+print(idd_wb)
 def create(type='enade'):
     workbook = enade_wb
     filename = 'novo_conceito_enade.xlsx'
     if type == 'idd':
+        print('we are on idd')
         filename = 'novo_conceito_idd.xlsx'
         workbook =  idd_wb
     enade_ws = workbook.active
@@ -51,7 +60,10 @@ def create(type='enade'):
 
     for i in alc:
         new_enade_ws[f'{i}1'].font = copy(enade_ws[f'{i}1'].font)
-        new_enade_ws[f'{i}1'].style = copy(enade_ws[f'{i}1'].style)
+        if year != '2018':
+            new_enade_ws[f'{i}1'].style = copy(enade_ws[f'{i}1'].style)
+        else:
+            new_enade_ws[f'{i}1'].style = copy(dead_style)
         new_enade_ws[f'{i}1'].border = copy(enade_ws[f'{i}1'].border)
         new_enade_ws[f'{i}1'].alignment = copy(enade_ws[f'{i}1'].alignment)
     #print(enade_ws.rows)

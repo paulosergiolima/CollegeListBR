e6ae4726b9ba0ad01ee602424e9362f7ed08e4df
Paulo Sergio Campos de Lima <cloverfieldor@gmail.com>
2023-01-20 14:17:59 -0300

    Working with docker now

--- static/script.js
+++ static/script.js
@@ -1,5 +1,5 @@
 function myfunction() {
     year = document.getElementById('year').value
     courses = document.getElementById('courses').value
-    window.open(`http://127.0.0.1:5000/${year}/enade/${courses}`)
+    window.open(`${location.origin}/${year}/enade/${courses}`)
 }

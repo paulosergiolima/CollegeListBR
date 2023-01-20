ba2881a659ceca5eb204138b73691fba9b1e8da6
Paulo Sergio Campos de Lima <cloverfieldor@gmail.com>
2023-01-20 14:01:05 -0300

    Input completed

--- main.py
+++ main.py
@@ -1,9 +1,6 @@
-from copy import copy
-from string import ascii_uppercase as alc
-from flask import request, Flask, send_file
+from flask import request, Flask, send_file, render_template, url_for
 from college_workbook import create
 app = Flask(__name__)
-
 @app.route('/<path:year>/<path:type>/<path:raw_courses>', methods=['POST', 'GET'])
 def login(year, type, raw_courses):
     raw_courses = raw_courses.replace('_', ' ')
@@ -12,3 +9,6 @@ def login(year, type, raw_courses):
     print(raw_courses)
     file = create(type=type, year=year, courses=courses)
     return send_file(file)
+@app.route('/')
+def index():
+    return render_template('index.html')

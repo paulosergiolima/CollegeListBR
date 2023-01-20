8a783cccb890e4791d33aad701979210aba42d98
Paulo Sergio Campos de Lima <cloverfieldor@gmail.com>
2023-01-20 14:03:58 -0300

    Small bug fixed

--- static/script.js
+++ static/script.js
@@ -1,6 +1,5 @@
 function myfunction() {
     year = document.getElementById('year').value
     courses = document.getElementById('courses').value
-    console.log(document.getElementById('download_link').getAttribute('href'))
     window.open(`http://127.0.0.1:5000/${year}/enade/${courses}`)
 }
